6849aba5ce13b047c817a3bedf38ea72a6a0da0c
XQX3161 <1558505253@qq.com>
2026-09-09 15:53:16 +0800

    feat: 添加User用户实体类

---
+++ mall-common/src/main/java/com/macro/common/entity/User.java
@@ -0,0 +1,43 @@
+package com.macro.common.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+/**
+ * 用户实体类
+ */
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+public class User {
+
+    /**
+     * 用户ID
+     */
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 手机号
+     */
+    private String phone;
+
+    /**
+     * 创建时间
+     */
+    private Date createTime;
+}
