34dd16ab0f2f49cfb9dc7a4a0392d7a90473cdd3
XQX3161 <xqxxqx3161@gmail.com>
2026-09-09 15:24:54 +0800

    初始化mall-swarm微服务商城项目

---
+++ mall-common/src/main/java/com/macro/common/CommonPage.java
@@ -0,0 +1,14 @@
+package com.macro.common;
+
+import lombok.Data;
+
+import java.util.List;
+
+@Data
+public class CommonPage<T> {
+    private Integer pageNum;
+    private Integer pageSize;
+    private Integer totalPage;
+    private Long total;
+    private List<T> list;
+}
